b22ee7b2a9e9ce536a313af84aedf6eff3854391
Bill Peregoy <bill@peregoy.org>
2017-09-30 10:14:38 -0500

    Added basic page layout

--- src/Main.elm
+++ src/Main.elm
@@ -5,8 +5,14 @@ import Html.Attributes exposing (..)
 
 
 main =
-    div [ class "jumbotron" ]
-        [ div [ class "container text-center" ]
-            [ h1 [] [ text "Elm Todo List" ]
+    div []
+        [ div [ class "jumbotron" ]
+            [ div [ class "container text-center" ]
+                [ h1 [] [ text "Elm Todo List" ] ]
+            ]
+        , div
+            [ class "row" ]
+            [ div [ class "col-md-3" ] [ text "sidebar" ]
+            , div [ class "col-md-9" ] [ text "main content" ]
             ]
         ]
